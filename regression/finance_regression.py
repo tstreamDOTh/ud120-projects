387bbf53500ae0ed013dfd31d1ac9b329ea85d90
T Thiyagaraj <tstream.h@outlook.com>
2018-07-13 23:16:47 +0530

    Add simple regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -37,8 +37,10 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import  linear_model
+reg = linear_model.LinearRegression()
 
-
+reg.fit(feature_train, target_train)
 
 
 
